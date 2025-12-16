97fbf31fa5d2a651e43734789309773d42a199e2
noahfranciscoo <b2029nccfrancisco@pshs.edu.ph>
2025-12-16 22:00:30 +0800

    fixed the grmmar

--- public/Scripts/timeline.js
+++ public/Scripts/timeline.js
@@ -1,66 +1,69 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   const timeline = [
-    {
-      year: "1939",
-      event: `
-        <p>World War 2 begins with the Polish rejection of the Reich's demands over Danzig. Poland quickly falls due to the double invasion by the Soviet Union and Germany.</p>
-        <p>The invasion shocks Europe, and the world braces for a long conflict.</p>
-      `,
-      image: "../public/assets/poland_timeline.jpg",
-      alt: "Germany Invades Poland"
-    },
-    {
-      year: "1940",
-      event: `
-        <p>Germany turns west to the lowlands and France, and executes its Blitzkrieg, quickly capitulating Belgium, Luxembourg, and the Netherlands with relatively few casualties. The French were surprised by this invasion, as their defenses were centered on the region of Alsace-Lorraine. The British Expeditionary Forces, alongside French troops, were encircled in the city of Dunkirk, and Germany either killed or captured 200-300k troops as POWs.</p>
-        <br>
-        <p>Paris soon falls, and France surrenders to the German Reich. This devastates the British public, leading to Churchill's removal. The new Prime Minister signs an armistice with Germany, although the Middle East and African Theatres remain active. Denmark and Norway were also captured during this campaign.</p>
-      `,
-      image: "../public/assets/germaninvadeussr.png",
-      alt: "Blitzkrieg in France"
-    },
-    {
-      year: "1941",
-      event: `
-        <p>Operation Barbarossa / the invasion of the Soviet Union by the Germans, and Pearl Harbor happened as usual. Japan did worse during the Pacific War because the Allies weren't fighting in Europe. Japan is still able to take Indochina (Vietnam, Cambodia, Laos), but struggles to take the rest of SEA.</p>
-      `,
-      image: "../public/assets/france_timeline.jpg",
-      alt: "Operation Barbarossa"
-    },
-    {
-      year: "1942",
-      event: `
-        <p>The Germans quickly advance into the Soviet Union, taking key cities such as Kiev, Minsk, and Odessa, but their advance halts due to weather conditions. The war in China is still ongoing, with the Empire of Japan gaining some ground.</p>
-      `,
-      image: "../public/assets/japanchina.jpg",
-      alt: "German Advance into Soviet Union"
-    },
-    {
-      year: "1943",
-      event: `
-        <p>The French government in exile consolidates its land in West and Central Africa, as the German puppet state takes control of Algeria, France's North African colony. The Germans are at the gates of Moscow, and the battle for Moscow begins.</p>
-      `,
-      image: "../public/assets/franceinafrica.jpg",
-      alt: "French Exile in Africa"
-    },
-    {
-      year: "1944",
-      event: `
-        <p>Moscow is captured, and Stalin disappears. By the beginning of 1945, the Germans had mostly achieved their European goals, and the fighting had mostly stopped, although no peace treaty was ever signed. The revolution in India, sparked by the Bengal Famine, began, led by Subhas Chandra Bose. Henry A Wallace stayed as Roosevelt's VP instead of being replaced by Truman.</p>
-      `,
-      image: "../public/assets/moscow.jpg",
-      alt: "Capture of Moscow"
-    },
-    {
-      year: "1945",
-      event: `
-        <p>Henry Wallace became President after Roosevelt's death. The American invasion of Siberia begins after Japan refuses to surrender. Operation Olympic commences with an invasion of Kyushu, followed by the atomic bombings of Hiroshima, Nagasaki, Kokura, and Yokohama. Japan sues for peace.</p>
-      `,
-      image: "../public/assets/japanbombed.jpg",
-      alt: "End of WWII"
-    }
-  ];
+    
+
+  {
+    year: "1939",
+    event: `
+      <p>World War 2 begins with Poland rejecting the Reich's demands over Danzig. Poland quickly falls due to the double invasion by the Soviet Union and Germany.</p>
+      <p>The invasion shocks Europe, and the world braces for a long conflict.</p>
+    `,
+    image: "../public/assets/poland_timeline.jpg",
+    alt: "Germany Invades Poland"
+  },
+  {
+    year: "1940",
+    event: `
+      <p>Germany turns west to the Lowlands and France, and executes its Blitzkrieg, quickly capitulating Belgium, Luxembourg, and the Netherlands with relatively few casualties. The French are surprised by this invasion, as their defenses center on Alsace-Lorraine. The British Expeditionary Forces, alongside French troops, are encircled in Dunkirk, and Germany either kills or captures 200-300k troops as POWs.</p>
+      <br>
+      <p>Paris soon falls, and France surrenders to the German Reich. This devastates the British public, leading to Churchill's removal. The new Prime Minister signs an armistice with Germany, although the Middle East and African Theatres remain active. Denmark and Norway are also captured during this campaign.</p>
+    `,
+    image: "../public/assets/germaninvadeussr.png",
+    alt: "Blitzkrieg in France"
+  },
+  {
+    year: "1941",
+    event: `
+      <p>Operation Barbarossa, the German invasion of the Soviet Union, and Pearl Harbor occur as usual. Japan struggles during the Pacific War because the Allies are not fighting in Europe. Japan still takes Indochina (Vietnam, Cambodia, Laos) but struggles to take the rest of Southeast Asia.</p>
+    `,
+    image: "../public/assets/france_timeline.jpg",
+    alt: "Operation Barbarossa"
+  },
+  {
+    year: "1942",
+    event: `
+      <p>The Germans quickly advance into the Soviet Union, taking key cities such as Kiev, Minsk, and Odessa, but their advance halts due to weather conditions. The war in China continues, with the Empire of Japan gaining some ground.</p>
+    `,
+    image: "../public/assets/japanchina.jpg",
+    alt: "German Advance into Soviet Union"
+  },
+  {
+    year: "1943",
+    event: `
+      <p>The French government in exile consolidates its land in West and Central Africa, as the German puppet state takes control of Algeria, France's North African colony. The Germans reach the gates of Moscow, and the battle for Moscow begins.</p>
+    `,
+    image: "../public/assets/franceinafrica.jpg",
+    alt: "French Exile in Africa"
+  },
+  {
+    year: "1944",
+    event: `
+      <p>Moscow is captured, and Stalin disappears. By the beginning of 1945, the Germans mostly achieve their European goals, and the fighting mostly stops, although no peace treaty is signed. The revolution in India, sparked by the Bengal Famine, begins, led by Subhas Chandra Bose. Henry A Wallace stays as Roosevelt's VP instead of being replaced by Truman.</p>
+    `,
+    image: "../public/assets/moscow.jpg",
+    alt: "Capture of Moscow"
+  },
+  {
+    year: "1945",
+    event: `
+      <p>Henry Wallace becomes President after Roosevelt's death. The American invasion of Siberia begins after Japan refuses to surrender. Operation Olympic commences with an invasion of Kyushu, followed by the atomic bombings of Hiroshima, Nagasaki, Kokura, and Yokohama. Japan sues for peace.</p>
+    `,
+    image: "../public/assets/japanbombed.jpg",
+    alt: "End of WWII"
+  }
+];
+
 
   let index = 0;
 
